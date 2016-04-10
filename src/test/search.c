ed56516ff389d06fd9b183f53edbc79b5c7b81a3
Martin Lund <martin.lund@keep-it-simple.com>
2016-04-10 15:03:27 +0200

    Added test code

---
+++ src/test/search.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <lxi.h>
+
+void broadcast(char *address, char *interface)
+{
+    printf("Broadcasting on interface %s\n", interface);
+}
+
+void device(char *address, char *id)
+{
+    printf(" Found %s on address %s\n", id, address);
+}
+
+int main()
+{
+    struct lxi_info_t info;
+
+    // Initialize LXI library
+    lxi_init();
+
+    // Set up search information callbacks
+    info.broadcast = &broadcast;
+    info.device = &device;
+
+    printf("Searching for LXI devices - please wait...\n\n");
+
+    // Search for LXI devices, 1 second timeout
+    lxi_discover(&info, 1000);
+
+    return 0;
+}
